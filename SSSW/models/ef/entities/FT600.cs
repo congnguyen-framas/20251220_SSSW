2ce9f46768859f9b2273452c710faedea727abec
Cong Nguyen <68525123+congnguyen-framas@users.noreply.github.com>
2026-05-15 11:55:02 +0700

    updaeting

--- SSSW/models/ef/entities/FT600.cs
+++ SSSW/models/ef/entities/FT600.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SSSW.models
 {
-    [Table("FT600")]
+    [Table("FT600_Test")]
     public partial class FT600 : EntityCommon
     {
         [Browsable(false)][Key] public Guid id { get; set; }
